cefd68434962c3430f9e92ad95c668db564f789d
hiep-ph288 <hiep.ph288@gmail.com>
2022-09-17 15:36:12 +0700

    quizz

--- Main.tsx
+++ Main.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import CharacterBox from "./src/components/CharacterBox";
 import MemberItem, { MemberItemProps } from "./src/components/MemberItem";
 import SearchBar from "./src/components/SearchBar";
@@ -7,6 +7,7 @@ import Header from "./src/components/Header";
 import MemberImage from "./src/components/MemberImage";
 import Home from "./src/screens/Home";
 import CategoryInfo, { CategoryInfoProps } from "./src/components/CategoryInfo"
+import TextBox, { EStatus } from "./src/components/TextBox";
 
 
 const data: CategoryInfoProps[] = [
@@ -14,8 +15,27 @@ const data: CategoryInfoProps[] = [
   // { image: require("./assets/images/lion-item.png"), name: "Lion" },
 ];
 
+const questions = [
+  "Con trung",
+  "Vu",
+  "De trung"
+]
+
 export default function Main() {
-  return <CategoryInfo {...data[0]} />;
+  const [status, setStatus] = useState<EStatus[]>([EStatus.IN_CORRECT, EStatus.DISABLE, EStatus.CORRECT])
+  const onPress = (i: number) => () => {
+    const newStatus = [...status];
+    for (let index = 0; index < newStatus.length; index++) {
+      newStatus[index] = EStatus.NORMAL;
+    }
+    newStatus[i] = EStatus.DISABLE;
+    setStatus(newStatus)
+  }
+  console.log(status);
+  
+  return <View>
+    {questions.map((ques, i) => <TextBox key={ques} status={status[i]} onPress={onPress(i)} content={ques} />)}
+  </View>;
 }
 
 const styles = StyleSheet.create({});
