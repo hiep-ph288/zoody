57f16f52c2522bc78b7d03292817f2d3dd884ece
pth21012001 <toankhothat@gmail.com>
2022-09-17 15:30:51 +0700

    PartSummary fix

--- src/screens/Category.tsx
+++ src/screens/Category.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Stack } from 'native-base'
+import Header from '../components/Header'
 
 export default function Category() {
   return (
-    <View>
-      <Text>Category</Text>
-    </View>
+    <Stack>
+      <Header title='test' />
+    </Stack>
   )
 }
 
